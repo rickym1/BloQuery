e806ba5277a4fa0c10ff8e8b6b0e19467910fa31
Rick Matta <rickmatta@gmail.com>
2015-07-23 20:03:38 -0400

    added logout button and removed back button from login

--- bloQuery/bloQuery/LoginViewController.m
+++ bloQuery/bloQuery/LoginViewController.m
@@ -19,7 +19,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.navigationItem.hidesBackButton = YES;
+    
 }
 - (IBAction)loginTapped:(id)sender {
     
